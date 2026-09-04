544fe8bad41f7f3556fea8bcbf890e13a9cfe81e
Preoperacionalesindimon <preoperacionalesindimon@gmail.com>
2026-09-04 11:42:02 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
      la versión más nueva del formulario cuando hay señal).
    ============================================================ */
 
-const CACHE_NAME = 'ssta-portal-v9';
+const CACHE_NAME = 'ssta-portal-v10';
 
 const PAGES = [
   './',
